99a17ab6d217ac8abbbc3a34660d93a46d8b189c
Emad Almousa <almousa.emad.92@gmail.com>
2024-06-26 20:27:45 +0200

    Update the News component

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,17 +13,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-    <body className={inter.className}>
-      <div className='flex justify-between max-w-6xl mx-auto'>
-        <div  className='hidden sm:inline border-r h-screen'>
-          <Sidebar />
+      <body className={inter.className}>
+        <div className='flex justify-between max-w-6xl mx-auto'>
+          <div className='hidden sm:inline border-r h-screen'>
+            <Sidebar />
+          </div>
+          <div>{children}</div>
+          <div className='lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]'>
+            <div className='sticky top-0 bg-white py-2'>
+              <input
+                type='text'
+                placeholder='Search'
+                className='bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2'
+              ></input>
+            </div>
+            <News />
+          </div>
         </div>
-        <div>{children}</div>
-        <div>
-          <News />
-        </div>
-      </div>
-    </body>
-  </html>
+      </body>
+    </html>
   );
 }
